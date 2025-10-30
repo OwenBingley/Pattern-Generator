6be895b09778791620d16dfe9cb2f9dbeb4737b0
OwenBingley <owenbingley10@gmail.com>
2025-10-30 15:03:54 +0000

    save 1

--- Pattern.java
+++ Pattern.java
@@ -1,1 +1,50 @@
+//
+//
+//
+public class Pattern {
 
+    public static void stars(int rows) {
+    int i = 1; 
+    while (i <= rows) {
+        int j = 1; 
+        while (j <= (2 * i - 1)) { 
+            System.out.print("*");
+            j++;
+        }
+        System.out.println(); 
+        i++;
+    }
+}
+
+  public static void triangle(int rows) {
+    int i = 1; 
+    while (i <= rows) {
+        int j = 1; 
+        while (j <= i) {
+            System.out.print(i); 
+            j++;
+        }
+        System.out.println(); 
+        i++;
+    }
+}  
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static void main(String[] args) {
+        stars(5);
+         triangle();
+   
+    }
+
+}
